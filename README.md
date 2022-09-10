12edfb2578f25b3c6a64d17508b30b784749beaa
yejinzhan <jinzhan.ye@foxmail.com>
2022-09-10 11:11:38 +0800

    chore: 完成代码编写

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.bilibili.com/video/BV1st4y1v7yJ?spm_id_from=333.337.search-card.all.click&vd_source=59ea5ff1891eb61aaeb598d93e5abf80
+
+https://wizardforcel.gitbooks.io/chrome-doc/content/1.html
--- src/index.js
+++ src/index.js
@@ -2,24 +2,75 @@ function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-window.addEventListener('click', (evt) => {
+function handleClickUploadBtn() {
+  const doms = document.querySelectorAll('.download-slices__scales .ten-checkbox input');
+  let targetDom = null
+  const checkValues = [];
+
+  doms.forEach((item) => {
+    if (item.checked) {
+      targetDom = item
+      checkValues.push(item.value);
+    }
+  })
+
+  if (!checkValues.length) {
+    alert('请勾选上传图片');
+    return;
+  }
+
+  if (checkValues.length > 1) {
+    alert('一次只能上传一张图片');
+    return;
+  }
+
+  // https://cdn3.codesign.qq.com/dcloud/attachments/2022/04/24/2867be21a0825945b010454984f99409-o.png?imageMogr2/thumbnail/700x650/interlace/1
+  const imageSrc = document.querySelector('.node-box__content .thumb img').getAttribute('src')
+  const rawImageSrc = imageSrc.slice('?imageMogr2')[0]
+
+  // https://cdn3.codesign.qq.com/dcloud/attachments/2022/04/24/feb9a55daeb3a6ce77df056e370967f1-o.png?imageMogr2/thumbnail/200x200/quality/40/format/webp
+  const thumbnail = targetDom.parentElement.parentElement.querySelector('small').innerText.replace(/px|\s+/g, '')
+  const qualityText = document.querySelector('.download-slices .download-slices__item .ten-input__input-input').value
+  const matcher = qualityText.match(/\d+/g, '')
+  const format = document.querySelector('.download-slices .download-slices__type .ten-input__input-input').value.trim()
+
+  const imageParams = [
+    `thumbnail/${ thumbnail }`,
+    `matcher/${ matcher ? matcher[0] : 100 }`,
+    `format/${ format }`,
+  ].join('/')
+
+  const finalImageUrl = `${ rawImageSrc }?imageMogr2/${ imageParams }`
+
+  console.log('finalImageUrl:', finalImageUrl)
+
+  // TODO
+  // fetch('url', () => {
+  //   // 上传链接写入粘贴板
+  //   // 提示上传成功
+  // });
+}
+
+window.addEventListener('click', function handleClickLayer(evt) {
   const classText = evt.target.getAttribute('class')
   // 可能要异步判断，selected 应该是点击后 codesign 加上去的
   const isClickLayer = classText.includes('selected') && classText.includes('layer')
 
   if (isClickLayer) {
+    const uploadBtnDom = document.querySelector('#zaUploadBtn')
+
+    if (uploadBtnDom) {
+      return
+    }
+
     const downloadBtn = document.querySelector('.download-slices__confirm-button')
-    const uploadBtn = document.createElement('div');
-    uploadBtn.style = 'margin-top: 20px;';
-    uploadBtn.innerHTML = '上传图片';
-    uploadBtn.addEventListener('click', () => {
-      const imageThumb = document.querySelector('.node-box__content .thumb img')
-      // https://cdn3.codesign.qq.com/dcloud/attachments/2022/04/24/2867be21a0825945b010454984f99409-o.png?imageMogr2/thumbnail/700x650/interlace/1
-      const imageSrc = imageThumb.getAttribute('src')
-      const rawImageSrc = imageSrc.slice('?imageMogr2')[0]
+    const uploadBtn = document.createElement('div')
 
-    })
+    uploadBtn.id = 'zaUploadBtn'
+    uploadBtn.style = 'margin-top: 20px;'
+    uploadBtn.innerHTML = '上传图片'
+    uploadBtn.addEventListener('click', handleClickUploadBtn)
 
-    insertAfter(downloadBtn, uploadBtn);
+    insertAfter(downloadBtn, uploadBtn)
   }
 })

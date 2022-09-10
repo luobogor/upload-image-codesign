73fbfc24a6aa5aed549ff69d36a09b0b68c86c3f
yejinzhan <jinzhan.ye@foxmail.com>
2022-09-10 12:33:55 +0800

    chore: 调度中

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 https://www.bilibili.com/video/BV1st4y1v7yJ?spm_id_from=333.337.search-card.all.click&vd_source=59ea5ff1891eb61aaeb598d93e5abf80
 
 https://wizardforcel.gitbooks.io/chrome-doc/content/1.html
+
+url 匹配
+https://developer.chrome.com/docs/extensions/mv3/match_patterns/
+
+"https://codesign.qq.com/s/*/*",
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+function showToast() {
+
+}
+
 function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 function handleClickUploadBtn() {
-  const doms = document.querySelectorAll('.download-slices__scales .ten-checkbox input');
+  const checkerDoms = document.querySelectorAll('.download-slices__scales .ten-checkbox input');
   let targetDom = null
   const checkValues = [];
 
-  doms.forEach((item) => {
+  checkerDoms.forEach((item) => {
     if (item.checked) {
       targetDom = item
       checkValues.push(item.value);
@@ -26,23 +30,30 @@ function handleClickUploadBtn() {
 
   // https://cdn3.codesign.qq.com/dcloud/attachments/2022/04/24/2867be21a0825945b010454984f99409-o.png?imageMogr2/thumbnail/700x650/interlace/1
   const imageSrc = document.querySelector('.node-box__content .thumb img').getAttribute('src')
-  const rawImageSrc = imageSrc.slice('?imageMogr2')[0]
+  const rawImageSrc = imageSrc.split('?imageMogr2')[0]
 
   // https://cdn3.codesign.qq.com/dcloud/attachments/2022/04/24/feb9a55daeb3a6ce77df056e370967f1-o.png?imageMogr2/thumbnail/200x200/quality/40/format/webp
-  const thumbnail = targetDom.parentElement.parentElement.querySelector('small').innerText.replace(/px|\s+/g, '')
-  const qualityText = document.querySelector('.download-slices .download-slices__item .ten-input__input-input').value
-  const matcher = qualityText.match(/\d+/g, '')
-  const format = document.querySelector('.download-slices .download-slices__type .ten-input__input-input').value.trim()
+  const thumbnail = targetDom.parentElement.parentElement.querySelector('small')
+    .innerText
+    .replace(/px|\s+/g, '')
+  const qualityMatcher = document.querySelector('.download-slices .download-slices__quality .ten-input__input-input')
+    .value
+    .match(/\d+/g, '')
+  const quality = qualityMatcher ? qualityMatcher[0] : 100;
+  const format = document.querySelector('.download-slices .download-slices__format .ten-input__input-input')
+    .value
+    .toLowerCase()
 
   const imageParams = [
     `thumbnail/${ thumbnail }`,
-    `matcher/${ matcher ? matcher[0] : 100 }`,
+    `quality/${ quality }`,
     `format/${ format }`,
   ].join('/')
 
   const finalImageUrl = `${ rawImageSrc }?imageMogr2/${ imageParams }`
 
   console.log('finalImageUrl:', finalImageUrl)
+  showToast('finalImageUrl:' + finalImageUrl)
 
   // TODO
   // fetch('url', () => {
@@ -52,25 +63,24 @@ function handleClickUploadBtn() {
 }
 
 window.addEventListener('click', function handleClickLayer(evt) {
-  const classText = evt.target.getAttribute('class')
-  // 可能要异步判断，selected 应该是点击后 codesign 加上去的
-  const isClickLayer = classText.includes('selected') && classText.includes('layer')
+  const uploadBtnDom = document.querySelector('#zaUploadBtn')
 
-  if (isClickLayer) {
-    const uploadBtnDom = document.querySelector('#zaUploadBtn')
-
-    if (uploadBtnDom) {
-      return
-    }
-
-    const downloadBtn = document.querySelector('.download-slices__confirm-button')
-    const uploadBtn = document.createElement('div')
-
-    uploadBtn.id = 'zaUploadBtn'
-    uploadBtn.style = 'margin-top: 20px;'
-    uploadBtn.innerHTML = '上传图片'
-    uploadBtn.addEventListener('click', handleClickUploadBtn)
-
-    insertAfter(downloadBtn, uploadBtn)
+  if (uploadBtnDom) {
+    return
   }
+
+  const downloadBtn = document.querySelector('.download-slices__confirm-button')
+
+  if (!downloadBtn) {
+    return
+  }
+
+  const uploadBtn = document.createElement('div')
+
+  uploadBtn.id = 'zaUploadBtn'
+  uploadBtn.setAttribute('class', 'zhenai__upload-btn')
+  uploadBtn.innerHTML = '上传图片'
+  uploadBtn.addEventListener('click', handleClickUploadBtn)
+
+  insertAfter(downloadBtn, uploadBtn)
 })
